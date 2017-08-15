06a470a4235ff273cd59a0212dfc2155ef416b90
Cliff Albert <cliff.albert@gmail.com>
2017-08-15 12:17:36 +0200

    - Sanitized debugging code - Implemented 'strict'

--- fc-migrate.pl
+++ fc-migrate.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 
-my $version = "0.1";
+use strict;
+
+my $version = "0.2";
 
 print "Qlogic Sanbox to Brocade DCX Zoning Migration\n";
 print "Release $version\n";
@@ -28,6 +30,7 @@ my $debug = 0;
 
 sub convertWWN {
    my ($wwn) = @_;
+   my $outputWWN;
    if ($wwn =~ /^([0-9a-z]{2})([0-9a-z]{2})([0-9a-z]{2})([0-9a-z]{2})([0-9a-z]{2})([0-9a-z]{2})([0-9a-z]{2})([0-9a-z]{2})/) {
     $outputWWN = "$1:$2:$3:$4:$5:$6:$7:$8";
     return $outputWWN;
@@ -38,8 +41,8 @@ sub convertWWN {
 sub returnWWNorAlias {
    my ($WWNMember) = @_;
    my $applicable_alias = 0;
-   if ($wwnAlias{$zoneMemberDB[$row][$column]}) {
-	$applicable_alias = $wwnAlias{$zoneMemberDB[$row][$column]};
+   if ($wwnAlias{$WWNMember}) {
+	$applicable_alias = $wwnAlias{$WWNMember};
         $applicable_alias =~ s/-/_/g;
    } else {
 	$applicable_alias = convertWWN($WWNMember);
@@ -58,63 +61,61 @@ sub returnWWNorAlias {
 
    while (<CONFIG_FILE>) { 
 	if ($_ =~ /^NAME=Default.Snmp.SysDescr,TYPE=STRING,VAL=(.*$)/) {
-	   $sourcePlatform = $1;
+	   my $sourcePlatform = $1;
 	   print " - Migrating from $sourcePlatform\n";
         };
  	if ($_ =~ /^NAME=Global.Nicknames.NumberOfNicknames,TYPE=UINT,VAL=(\d+)/) {
- 	   $NumberOfNicknames = $1;
+ 	   my $NumberOfNicknames = $1;
 	   print " - Total number of WWN Nicknames is $NumberOfNicknames\n";
         };
 	if ($_ =~ /^NAME=Global.Nicknames.(\d+),TYPE=STRING,VAL=\[(\S+)\]\[(\w+)\]\[WWN\]\[(\d+)\]\[(\d+)\]/) {
-           $nickNameId = $1;
-	   $nickNameName = $2; 
-	   $nickNameWWN = $3;
- 	   #$nickNameSerial = $4; 
-	   #$nickNameDigit = $5;
+           my $nickNameId = $1;
+	   my $nickNameName = $2; 
+	   my $nickNameWWN = $3;
  	   if ($debug) { print " - WWN $nickNameId Host $nickNameName has WWN $nickNameWWN \n"; };
    	   $wwnAlias{$nickNameWWN} = $nickNameName;
 	   $nickNameName =~ s/-/_/g; 
 	   print $outputhandle "alicreate \"$nickNameName\", \"".convertWWN($nickNameWWN)."\" \n";
 	};
 	if ($_ =~ /^NAME=Global.Zoning.NumberOfZones,TYPE=UINT,VAL=(\d+)/) { 
-	   $NumberOfZones = $1;
+	   my $NumberOfZones = $1;
 	   print " - Total number of Zones is $NumberOfZones (including orphaned zones)\n";
         };
 	if ($_ =~ /^NAME=Global.Zoning.ZoneList.(\d+),TYPE=STRING,VAL=(\S+)/) {
-	   $zoneId = $1;
-	   $zoneName = $2;
+	   my $zoneId = $1;
+	   my $zoneName = $2;
 	   if ($debug) { print " - Zone $zoneId is $zoneName \n"; };
 	   $zoneDB[$zoneId] = $zoneName;
 	}; 
 	if ($_ =~ /^NAME=Global.Zoning.Zone.(\d+).ZoneType,TYPE=STRING,VAL=(\S+)/) {
-	   $zoneTypeId = $1;
-	   $zoneTypeType = $2;
+	   my $zoneTypeId = $1;
+	   my $zoneTypeType = $2;
 	   if ($debug) { print " - Zone $zoneDB[$zoneTypeId] is $zoneTypeType Zoned\n"; };
 	   $zoneTypeDB[$zoneTypeId] = $zoneTypeType;
 	};
-	if ($_ =~ /^NAME=Global.Zoning.Zone.(\d+).NumberOfZoneMembers,TYPE=UINT,VAL=(\d+)/) {
-	   $zoneCountId = $1;
-	   $zoneCountNum = $2;
-	   if ($debug) { print " - Zone $zoneDB[$zoneCountId] has $zoneCountNum members\n"; }; 
+	if ($debug && $_ =~ /^NAME=Global.Zoning.Zone.(\d+).NumberOfZoneMembers,TYPE=UINT,VAL=(\d+)/) {
+	   my $zoneCountId = $1;
+	   my $zoneCountNum = $2;
+	   print " - Zone $zoneDB[$zoneCountId] has $zoneCountNum members\n"; 
 	};
  	if ($_ =~ /^NAME=Global.Zoning.Zone.(\d+).ZoneMemberList.(\d+),TYPE=STRING,VAL=1(\S+)/) {	
-	   $zoneMemberZone = $1;
-	   $zoneMemberId = $2;
-	   $zoneMemberWWN = $3;
+	   my $zoneMemberZone = $1;
+	   my $zoneMemberId = $2;
+	   my $zoneMemberWWN = $3;
 	   if ($debug) { print " - Zone $zoneDB[$zoneMemberZone] Member $zoneMemberId has WWN ".$wwnAlias{$zoneMemberWWN}."($zoneMemberWWN)\n"; };
  	   push @{$zoneMemberDB[$zoneMemberZone]}, $zoneMemberWWN;
 	};
-	if ($_ =~ /^NAME=Global.Zoning.ZoneSet.(\d+).ZoneIndex.(\d+),TYPE=UINT,VAL=(\d+)/) {
- 	   $zoneSetId = $1;
-	   $zoneSetIndex = $2;
-	   $zoneSetZoneId = $3;
-	   if ($debug) { print " - ZoneSet $zoneSetId has $zoneSetZoneId on index $zoneSetIndex\n"; };
+	if ($debug && $_ =~ /^NAME=Global.Zoning.ZoneSet.(\d+).ZoneIndex.(\d+),TYPE=UINT,VAL=(\d+)/) {
+ 	   my $zoneSetId = $1;
+	   my $zoneSetIndex = $2;
+	   my $zoneSetZoneId = $3;
+	   print " - ZoneSet $zoneSetId has $zoneSetZoneId on index $zoneSetIndex\n";
         }; 
 
         if ($_ =~ /^NAME=Global.Zoning.ZoneSet.(\d+).ZoneList.(\d+),TYPE=STRING,VAL=(\S+)/) {
-	   $zoneSetListId = $1;
-	   $zoneSetListListId = $2;
-	   $zoneSetListListName = $3;
+	   my $zoneSetListId = $1;
+	   my $zoneSetListListId = $2;
+	   my $zoneSetListListName = $3;
 	   if ($debug) { print " - ZoneSet $zoneSetListId has $zoneSetListListName on $zoneSetListListId\n"; };
  	   $zoneSetDB{$zoneSetListListName} = $zoneSetListListId;
 	};
@@ -125,12 +126,15 @@ sub returnWWNorAlias {
 
    my $fabric_created = 0;
 
+   my $row;
    foreach $row (0..@zoneMemberDB-1) {
 	if ($debug) { print "D: $zoneDB[$row] \n"; }
 	if (defined $zoneSetDB{$zoneDB[$row]}) {
-          $applicable_zone = $zoneDB[$row];
+          my $applicable_zone = $zoneDB[$row];
+  	  my $command_output;
   	  $applicable_zone =~ s/-/_/g;
 	  $command_output = "zonecreate \"$applicable_zone\",\"";
+ 	  my $column;
  	  foreach $column (0..@{$zoneMemberDB[$row]}-1) {
             $command_output .= " ".returnWWNorAlias($zoneMemberDB[$row][$column]).";";
  	    if ($debug) { print "Zone $zoneDB[$row] has member $wwnAlias{$zoneMemberDB[$row][$column]} ($zoneMemberDB[$row][$column])\n"; };
